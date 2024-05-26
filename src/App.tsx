76309dce9e96f19e2633b890ed75f4e200c897b5
Poroburu <poroburu@gmail.com>
2024-05-26 05:39:58 -0400

    pumpdotfunosmo ptrump wormhole connect bridge proof of concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import WormholeConnect, {
+  WormholeConnectConfig,
+} from "@wormhole-foundation/wormhole-connect";
 
+const config: WormholeConnectConfig = {
+  network: "mainnet",
+  chains: ["solana", "osmosis"],
+  rpcs: {
+    solana: "https://solana-mainnet.core.chainstack.com/b6cc78ce81eaaaf59554e341f60bb6b3",
+    osmosis: "https://osmosis-rpc.publicnode.com:443",
+    wormchain: "https://wormchain-rpc.polkachu.com/",
+  },
+  tokens: ["SOL", "PTRUMP"],
+  tokensConfig: {
+    "PTRUMP": {
+      key: "PTRUMP",
+      symbol: "PTRUMP",
+      nativeChain: "solana",
+      tokenId: {
+        "chain": "solana",
+        "address": "Gk2kRrwNMBU4Dn9JhC1Dks8G5X9nqi4ZE5jMvK6bdgEd"
+      },
+      coinGeckoId: "pepe-trump",
+      "icon": "https://assets.coingecko.com/coins/images/37560/standard/Icon_2.png?1714872973",
+      decimals: {
+        default: 6,
+      },
+      foreignAssets: {
+        "osmosis": {
+          "address": "ibc/B2A43A2B6BD7E5E81F1E67D60C56C0B72EABB31E45DD32BF1E96B124EE75404D",
+          "decimals": 5,
+        }
+      },
+    },
+  },
+  bridgeDefaults: {
+    fromNetwork: "solana",
+    toNetwork: "osmosis",
+    token: "PTRUMP",
+  }
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WormholeConnect config={config} />
     </div>
   );
 }
