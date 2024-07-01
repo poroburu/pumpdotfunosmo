742c686273c705cfcc2dde4c229ee0e779740cc4
Poroburu <poroburu@gmail.com>
2024-06-30 23:40:39 -0400

    refactor to PBJ

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,37 +5,31 @@ const config: WormholeConnectConfig = {
     chains: ["solana", "osmosis"],
     rpcs: {
         solana: "https://solana-mainnet.core.chainstack.com/b6cc78ce81eaaaf59554e341f60bb6b3",
-        osmosis: "https://osmosis-rpc.publicnode.com:443",
+        osmosis: "https://rpc.osmosis.zone:443",
         wormchain: "https://wormchain-rpc.polkachu.com/",
     },
-    tokens: ["SOL", "PTRUMP"],
+
+    tokens: ["PBJ", "SOL", "WSOL"],
     tokensConfig: {
-        PTRUMP: {
-            key: "PTRUMP",
-            symbol: "PTRUMP",
+        PBJ: {
+            key: "PBJ",
+            symbol: "PBJ",
             nativeChain: "solana",
             tokenId: {
                 chain: "solana",
-                address: "Gk2kRrwNMBU4Dn9JhC1Dks8G5X9nqi4ZE5jMvK6bdgEd",
+                address: "ANu4Wuq86WzRU8tykszQUJ66eQzFNfkwap2HcQ5UaFaU",
             },
-            coinGeckoId: "pepe-trump",
-            icon: "https://assets.coingecko.com/coins/images/37560/standard/Icon_2.png?1714872973",
+            coinGeckoId: "",
+            icon: "https://ipfs.io/ipfs/QmY6bPur4fJTpzbF3FHh7Xq1bpipi2QFGGyk3QKjEtCfQL",
             decimals: {
                 default: 6,
-            },
-            foreignAssets: {
-                osmosis: {
-                    address:
-                        "ibc/B2A43A2B6BD7E5E81F1E67D60C56C0B72EABB31E45DD32BF1E96B124EE75404D",
-                    decimals: 5,
-                },
             },
         },
     },
     bridgeDefaults: {
         fromNetwork: "solana",
         toNetwork: "osmosis",
-        token: "PTRUMP",
+        token: "PBJ",
     },
 };
 export default config;
