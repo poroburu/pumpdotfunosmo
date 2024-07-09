dc485caa7d58044835eb943a85aab405bdd11cab
Poroburu <poroburu@gmail.com>
2024-07-09 01:38:44 -0400

    Add React Router to scaffold URL bridge paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
 import './App.css';
 import WormholeConnect from "@wormhole-foundation/wormhole-connect";
 import theme from "./theme/theme";
 import config from "./config/config";
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
 function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" Component={Home} />
+      </Routes >
+    </BrowserRouter>
+  )
+}
+
+function Home() {
   return (
     <div className="App">
       <WormholeConnect config={config} theme={theme} />
